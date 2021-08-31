f540df1cd0b807130a1201f037345566fa1d2eed
IvanFernandez16 <ivan.leonel.fernandez16@gmail.com>
2021-08-31 00:19:48 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+
+function look_For_Titles(youtuber) {
+  (async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/");
+    await page.waitForSelector(".ytd-searchbox");
+    await page.click(".ytd-searchbox");
+    await page.type(".ytd-searchbox", youtuber);
+    await page.click("#search-icon-legacy");
+    await page.waitForSelector(".ytd-channel-renderer");
+    await page.click(".ytd-channel-renderer #avatar #img");
+    await page.waitForSelector(
+      "#tabsContent > tp-yt-paper-tab:nth-child(4)"
+    ); /*
+      await page.click("#accept-button");*/
+    await page.click("#tabsContent > tp-yt-paper-tab:nth-child(4)");
+    await page.waitForSelector("ytd-grid-renderer");
+    const titulos = await page.evaluate(() => {
+      const elements = document.querySelectorAll(
+        "#contents > ytd-grid-renderer #video-title"
+      );
+      const titles = [];
+
+      for (let element of elements) {
+        titles.push(element.innerText);
+      }
+      return titles;
+    });
+
+    console.log(titulos);
+  })();
+}
+
+look_For_Titles("coderhood");
